f6e498ed861cb37e4e6a71968b061aaaaf75e9a3
MarsLig <98904706+MartialLig@users.noreply.github.com>
2023-02-18 18:45:08 +0100

    Last version

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from LaunchGame  import LaunchGame
 from IA_NeuralNetwork import nn_alpha_zero, neural_network_agent
 from Players import alpha_zero_agent, MockModel_random,MockModel_greedy,HumanPlayer
 from GenerationAndLearn import dataset_generation_and_learn
-
+from keras.models import load_model
 
 
 
@@ -40,8 +40,50 @@ class main:
     
 
 
-launch = main() 
-launch.launch_training()
+
+def launch_learning_from_begining():
+    #launch learning from begining,  neural networks are saved in the file
+    launch = main() 
+    launch.launch_training()
+    return
+
+
+#To launch learning, uncomment the line :
+#launch_learning_from_begining()
+def launch_a_game_to_play(model_number):
+    #1 is human
+    #2 is greedy model
+    #3 is random model
+    path = "neural_network15"
+    nn = load_model(path)
+    nn2 = neural_network_agent()
+    nn2.model = nn
+    nn_alpha = nn_alpha_zero( nn2, epochs = 10, batch_size = 64)
+
+    game = Othello()
+
+    MCTS_new_agent = MonteCarloTreeSearch(5, nn_alpha, game)
+            
+    new_agent =  alpha_zero_agent(game, MCTS_new_agent)
+
+    player1 = new_agent# MockModel_greedy(game)
+    if model_number == 2 :
+        print("greedy model")
+        player2 = MockModel_greedy(game) 
+    elif model_number == 3 :
+        player2 = MockModel_random(game)
+        print("random model")
+    else :
+        player2 = HumanPlayer(game) 
+
+    launcher = LaunchGame(game, new_agent, player2)
+    launcher.launchTwoPlayers(1)
+    print("alpha zero is player 1")
+    return
+
+#To launch a game of alpha zero against another agent (human, greedy or random), uncomment the line :
+launch_a_game_to_play(3)
+        
 
 
 
